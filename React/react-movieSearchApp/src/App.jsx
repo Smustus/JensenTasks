6ae08e6e326983871f9c6b0c99d40502e140630c
Smustus <143539825+Smustus@users.noreply.github.com>
2024-03-07 11:21:49 +0100

    added code
    reformated code to increase reusability

--- React/react-movieSearchApp/src/App.jsx
+++ React/react-movieSearchApp/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import SearchMovies from './assets/components/SearchMovies/SearchMovies.jsx'
+import DisplayMovies from './assets/components/DisplayMovies.jsx';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
   
 
   return (
     <>
-      <SearchMovies />
+      <SearchMovies setMovies={ setMovies } />
+      <DisplayMovies movies={ movies }/>
     </>
   )
 }
